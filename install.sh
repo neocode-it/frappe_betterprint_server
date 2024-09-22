f12ffc3b3b7ac68cc7b296e88a7cb8f67e3e7849
Martin Heini <maheini35@me.com>
2024-09-22 15:47:05 +0200

    new path

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 apt-get update && apt-get install sudo git curl -y
 
 # Add bin dir of the tool to environment variables
-echo 'export "$HOME/.local/bin"' >> ~/.bashrc
-echo 'export "$HOME/.local/bin"' >> ~/.profile
+echo 'export PATH="$HOME/.local/bin"' >> ~/.bashrc
+echo 'export PATH="$HOME/.local/bin"' >> ~/.profile
 export PATH="$PATH:$HOME/.local/bin"
 
 # Install UV
@@ -13,5 +13,3 @@ curl -LsSf https://astral.sh/uv/install.sh | sh && source $HOME/.cargo/env
 
 # Install tool
 uv tool install ./Playwright-Server/dist/*.whl
-
-https://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@raw.githubusercontent.com/Neofix-IT/Playwright-Server/refs/heads/main/install.sh
