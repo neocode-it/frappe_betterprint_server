bb19bd1910db2071040d00e6e6d76caaf6ee4e3a
Martin Heini <maheini35@me.com>
2024-09-22 19:32:23 +0200

    advanced test :9

--- playwright_server.py
+++ playwright_server.py
@@ -1,3 +1,7 @@
+from playwright.sync_api import sync_playwright
+
+
+
 def main():
     print("Hello from playwright-server!")
 
@@ -7,4 +11,11 @@ if __name__ == "__main__":
 
 
 def test():
-    print('Hello....... test')
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto("http://playwright.dev")
+        print(page.title())
+        browser.close()
+
+    print('Finished :)')
