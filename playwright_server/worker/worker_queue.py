611dcc8fbb40afa303a49d47731165834be6e7e2
Martin Heini <maheini35@me.com>
2024-09-25 22:47:01 +0200

    worker queue - class to share data between threads

---
+++ playwright_server/worker/worker_queue.py
@@ -0,0 +1,30 @@
+import threading
+import queue
+
+class WorkerQueue:
+    _result = {}
+    _result_condition = threading.Condition()
+
+    _q = queue.Queue()
+    
+    def run_and_wait(self, command, content, key):
+        self._q.put({
+            "key": key,
+            "command": command,
+            "content": content
+        })
+
+        with self._result_condition:
+            while not key in self._result:
+                self._result_condition.wait()
+            result = self._result[key]
+            self._result.pop(key)
+            return result
+
+    def get_task(self):
+        return self._q.get(timeout=1)
+
+    def task_done(self, task, result):
+        with self._result_condition:
+            self._result[task["key"]] = result
+            self._result_condition.notify_all()
