5d3fae3945a17ef22326750a88696419e01287a9
Martin Heini <maheini35@me.com>
2024-09-27 00:00:22 +0200

    added logger

---
+++ playwright_server/logging/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+
+def setup_logger():
+    log_directory = "/var/log/myapp"
+    log_file = os.path.join(log_directory, "app.log")
+
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+def log(exc):
+    setup_logger()
+    logging.exception("Exception occurred: ", exc_info=exc)
