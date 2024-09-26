9f258bf5c243ea5fd9783b48f95cee5432dc91e4
Martin Heini <maheini35@me.com>
2024-09-26 23:11:30 +0200

    webserver skeleton

---
+++ playwright_server/server/start_server.py
@@ -0,0 +1,27 @@
+from waitress import serve
+
+from filelock import FileLock, Timeout
+import threading
+
+from playwright_server.worker.worker import worker
+from playwright_server.server.application import application
+import playwright_server.global_queue as global_queue
+
+
+def start_server(public=False):
+    lockfile = '/var/lock/playwright_server.lock'
+    lock = FileLock(lockfile)
+
+    try:
+        
+        with lock.acquire(blocking=False):
+            print('Serving App on localhost using port 39584. Press Ctrl. + C to stop...')
+
+            # Launch playwright worker thread
+            threading.Thread(target=worker_backbone, daemon=True, args=(global_queue.queue,)).start()
+            threading.Thread(target=worker_backbone, daemon=True, args=(global_queue.queue,)).start()
+
+
+    except Timeout:
+        print("Cannot start server: Another instance of Playwright Server currently holds the lock.")
+        exit(1)
