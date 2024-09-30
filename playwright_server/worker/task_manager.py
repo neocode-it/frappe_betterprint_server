cbb2f33b3294cfca9425380f4c997e1f035d1f18
Martin Heini <maheini35@me.com>
2024-09-30 17:49:39 +0200

    added task manager function

---
+++ playwright_server/worker/task_manager.py
@@ -0,0 +1,12 @@
+import re
+import html
+
+from playwright.sync_api import sync_playwright
+
+def manage_task(task, browser):
+    else:
+        return {
+            "error": True,
+            "description": "WORKER ERROR: Command not found"
+        }
+
