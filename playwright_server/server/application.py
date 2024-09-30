5fde80939e99a483fbe1eb412565ade2678aa2ef
Martin Heini <maheini35@me.com>
2024-09-30 18:15:38 +0200

    add basic server status und handling functionality

--- playwright_server/server/application.py
+++ playwright_server/server/application.py
@@ -13,6 +13,11 @@ def application(environ, start_response):
     if lenght and lenght < 10_000_000:
         data = request.get_json()
 
+    # Set default response
+    response = Response('Internal server error', status=500)
+    
+    if request.path == '/v1/status':
+        response = Response('PLAYWRIGHT OK', mimetype='text/plain')
 
     else:
         response = Response('Not Found', status=404)
