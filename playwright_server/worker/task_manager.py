3c6f51e61a68e52c774acd7d790bab450c5620dc
Martin Heini <maheini35@me.com>
2024-10-04 11:07:44 +0200

    add task to calculate element heights

--- playwright_server/worker/task_manager.py
+++ playwright_server/worker/task_manager.py
@@ -4,9 +4,26 @@ import html
 from playwright.sync_api import sync_playwright
 
 def manage_task(task, browser):
+    if task['command'] == 'calculate-element-heights':
+        return calculate_element_heights(task, browser)
     else:
         return {
             "error": True,
             "description": "WORKER ERROR: Command not found"
         }
 
+def calculate_element_heights(task, browser) -> dict:
+    html = html_wrapper(task['html'], strip_tags(task['style']))
+    page = browser.new_page()
+    page.set_content(html)
+    heights = page.evaluate('''
+        (selector) => {
+        const elements = document.querySelectorAll(selector);
+        const heights = [];
+        elements.forEach(element => {
+            heights.push(Math.round(element.offsetHeight * 0.26458));
+        });
+        return heights;
+    }''', strip_tags(task['element']))
+    page.close()
+    return {'content': heights}
