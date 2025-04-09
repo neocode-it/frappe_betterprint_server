2fc9588230294629303e8b7a4e43d5ee938fd5ec
Martin Heini <maheini35@me.com>
2025-04-09 12:23:07 +0200

    feat: allow cors for domain

--- betterprint_server/worker/task_manager.py
+++ betterprint_server/worker/task_manager.py
@@ -69,13 +69,13 @@ def generate_betterprint_pdf(task: dict, browser) -> dict:
     page = browser.new_page()
 
     # # Convert origin url into origin domain
-    # parsed_url = urlparse(task["allow_origin"])
-    # full_domain = parsed_url.netloc
-    # domain = full_domain.split(":")[0]  # Remove the port if present
+    parsed_url = urlparse(task["allow_origin"])
+    full_domain = parsed_url.netloc
+    domain = full_domain.split(":")[0]  # Remove the port if present
 
     # # Ignore CORS for this domain
     # # Workaround for: Chrome will always block CORS for local html files
-    # playwright_add_cors_allow_route(page, domain)
+    playwright_add_cors_allow_route(page, domain)
 
     # Add page content
     page.set_content(task["html"])
