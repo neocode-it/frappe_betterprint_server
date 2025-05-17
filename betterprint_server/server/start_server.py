3810b6d67c0a8621a681281d2f123d513c168015
maheini <maheini35@me.com>
2025-05-17 14:16:19 +0200

    remove lockfile

--- betterprint_server/server/start_server.py
+++ betterprint_server/server/start_server.py
@@ -9,28 +9,26 @@ import betterprint_server.global_queue as global_queue
 
 
 def start_server(public=False):
-    lockfile = "/var/lock/betterprint_server.lock"
-    lock = FileLock(lockfile)
+    # lockfile = "/var/lock/betterprint_server.lock"
+    # lock = FileLock(lockfile)
 
     try:
-        with lock.acquire(blocking=False):
-            print(
-                "Serving App on localhost using port 39584. Press Ctrl. + C to stop..."
-            )
+        # with lock.acquire(blocking=False):
+        print("Serving App on localhost using port 39584. Press Ctrl. + C to stop...")
 
-            # Launch playwright worker thread
-            threading.Thread(
-                target=worker_backbone, daemon=True, args=(global_queue.queue,)
-            ).start()
-            threading.Thread(
-                target=worker_backbone, daemon=True, args=(global_queue.queue,)
-            ).start()
+        # Launch playwright worker thread
+        threading.Thread(
+            target=worker_backbone, daemon=True, args=(global_queue.queue,)
+        ).start()
+        threading.Thread(
+            target=worker_backbone, daemon=True, args=(global_queue.queue,)
+        ).start()
 
-            # Launch http server as interface
-            if public:
-                serve(application, host="0.0.0.0", port=39584)
-            else:
-                serve(application, host="127.0.0.1", port=39584)
+        # Launch http server as interface
+        if public:
+            serve(application, host="0.0.0.0", port=39584)
+        else:
+            serve(application, host="127.0.0.1", port=39584)
 
     except Timeout:
         print(
